1df6abf3cf00b00fd17be44f49993e3f9931194e
carl-leonador03 <caleonador@my.cspc.edu.ph>
2025-05-29 01:38:24 +0800

    Removed unneeded import

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for, request, jsonify
+from flask import Flask, render_template, request, jsonify
 from base64 import b64decode, b64encode
 
 from sym import des, aes, blowfish, idea
